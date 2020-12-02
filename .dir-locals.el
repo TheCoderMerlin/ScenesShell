674e529edb07edabb8443daae0ebe1718888cc2c
David Ben-Yaakov <david@tangogolfdigital.com>
2020-12-02 12:16:24 -0600

    Bumped dylib versions

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((swift-mode
-  (flycheck-swift-include-search-paths  "/usr/local/lib/merlin/Igis-1.3.0/Igis/.build/debug" "/usr/local/lib/merlin/Scenes-1.0.5/Scenes/.build/debug")))
